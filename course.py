63f2ae8651fc1456ed63e8f3bf8c695092474701
MohamedGamal-Ahmed <mgamal.ahmed@outlook.com>
2025-06-15 12:09:25 +0300

    add student system

--- course.py
+++ course.py
@@ -1,27 +1,26 @@
-from system_manager import *
-
-
-class Course :
+# from system_manager import *
+#
+class Course:
     _id_counter = 1
-    def __init__(self , name):
+    def __init__(self, name):
         self.course_id = Course._id_counter
-        Course._id_counter += 1 
+        Course._id_counter += 1
         self.name = name
         self.enrolled_students = []
-
+        
+        
     def __str__(self):
-        return f"student id :{self.student_id}, name:{self.name}, Enrolled:{len(self.enrolled_students)}"
-    
-    def enroll_student(self , student):
-        if student not in self.enroll_student:
-            self.enroll_student.append(student)
-            print("student enrolled successfully .")
+        return f"Course ID: {self.course_id}, Name: {self.name}, Enrolled: {len(self.enrolled_students)}"
+
+    def enroll_student(self, student):
+        if student not in self.enrolled_students:
+            self.enrolled_students.append(student)
+            print("Student enrolled successfully.")
         else:
-            print("Student already enrolled .")
+            print("Student already enrolled.")
 
-    def remove_student(self , student):
-        for course in self.courses.values():
-            if student in course.enroll_student:
-                course.enroll_student.remove(student)
-
+    def remove_student(self, student):
+        for course in self.Courses.values():
+            if student in course.enrolled_students:
+                course.enrolled_students.remove(student)
 
--- student.py
+++ student.py
@@ -1,20 +1,19 @@
-class Student :
+class Student:
     _id_counter = 1
 
-    def __init__(self , name) :
+    def __init__(self, name):
         self.student_id = Student._id_counter
-        Student._id_counter +=1
+        Student._id_counter += 1
         self.name = name
         self.grades = {}
         self.enrolled_courses = []
-    
+        
 
     def __str__(self):
-        return f"student id :{self.student_id}, name:{self.name}, grades{(self.grades)}"
+        return f"Student ID: {self.student_id}, Name: {self.name}, Grades: {(self.grades)}"
 
-    def add_grade (self , course_id , grade):
+    def add_grade(self, course_id, grade):
         self.grades[course_id] = grade
-    
-    def enroll_in_course(self , course):
-        self.enrolled_courses.append(course)
 
+    def enroll_in_course(self, course):
+        self.enrolled_courses.append(course)
